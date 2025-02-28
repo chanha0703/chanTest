b7edae42fe374a84f051edc453e53fa20678469a
chan.hwang <chan.hwang@gsitm.com>
2025-02-28 10:52:32 +0900

    effect 추가

---
+++ src/main/java/kr/co/kgc/temp/biz/global/controller/GlobalController.java
@@ -0,0 +1,18 @@
+package kr.co.kgc.temp.biz.global.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GlobalController {
+  
+  @GetMapping("")
+  public String goMain() {
+    return "index";
+  }
+  
+  @GetMapping("/list")
+  public String getList() {
+    return "/document/list";
+  }
+}
